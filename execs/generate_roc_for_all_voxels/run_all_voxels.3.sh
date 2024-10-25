ad9ed319385bfcb3d2e9d78e3cccd846d186e3ee
maichi98 <elaichi.mohammed.1998@gmail.com>
2024-10-25 13:51:15 +0200

    stuff

---
+++ execs/generate_roc_for_all_voxels/run_all_voxels.3.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This script runs the ROC generation for all voxels for lesser priority setup 3
+
+# Ensure that the script exits if a command fails
+set -e
+
+# Activate the conda environment
+source "$(conda info --base)"/etc/profile.d/conda.sh
+conda activate env_relapse_prediction
+
+# Define the project directory:
+DIR_PROJECT="/home/maichi/work/my_projects/AIDREAM/RELAPSE_PREDICTION"
+
+# ROC for MRI features for high priority labels for feature mean_5x5x5:
+python "$DIR_PROJECT/relapse_prediction/roc/mri_roc.py" \
+        --labels "L3R" "L3R_5x5x5" "L2" "L2_5x5x5" "L3R - (L1 + L3)" "L3R - (L1 + L3)_5x5x5" \
+        --feature "mean_5x5x5" \
+        --voxel_strategy "all_voxels"\
+        --mp  --num_workers 3
+
+
+# ROC for high priority cercare features for high priority labels for feature mean_5x5x5:
+python "$DIR_PROJECT/relapse_prediction/roc/cercare_roc.py" \
+        --cercare_maps "CTH" "OEF" "rCBV" \
+        --feature "mean_5x5x5" \
+        --labels "L3R" "L3R_5x5x5" "L2" "L2_5x5x5" "L3R - (L1 + L3)" "L3R - (L1 + L3)_5x5x5" \
+        --voxel_strategy "all_voxels"\
+        --mp  --num_workers 3
+
+# ROC for MRI features for less priority labels for feature mean_5x5x5:
+python "$DIR_PROJECT/relapse_prediction/roc/mri_roc.py" \
+        --labels "L5" "L5_5x5x5" "L3" "L3_5x5x5" "L3 + L3R" "L3 + L3R_5x5x5" "L1" "L1_5x5x5" "L4" "L4_5x5x5"\
+        --feature "mean_5x5x5" \
+        --voxel_strategy "all_voxels"\
+        --mp  --num_workers 3
+
+
+# ROC for high priority cercare features for less priority labels for feature mean_5x5x5:
+python "$DIR_PROJECT/relapse_prediction/roc/cercare_roc.py" \
+        --cercare_maps "CTH" "OEF" "rCBV" \
+        --feature "mean_5x5x5" \
+        --labels "L5" "L5_5x5x5" "L3" "L3_5x5x5" "L3 + L3R" "L3 + L3R_5x5x5" "L1" "L1_5x5x5" "L4" "L4_5x5x5"\
+        --voxel_strategy "all_voxels"\
+        --mp  --num_workers 3
+
+# Deactivate the conda environment
+conda deactivate
